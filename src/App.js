ec763ffb4a1213e5b83dc3a1bbae2d3b8a902373
obloow <obloow.user@gmail.com>
2019-06-23 21:46:31 +0200

    authentication mail

--- src/App.js
+++ src/App.js
@@ -1,26 +1,75 @@
-import React from 'react';
+import React, { Component, Fragment } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import firebase from './firebase';
+
+class App extends Component {
+  state = {
+    email: '',
+    password: '',
+    user: ''
+  }
+
+  signIn = async () => {
+    await firebase.auth().signInWithEmailAndPassword(this.state.email, this.state.password)
+      .then(() => {
+        this.setState({ user: firebase.auth().currentUser })
+      })
+      .catch(function (error) {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        console.log(errorCode, errorMessage)
+      });
+    console.log('Connecté')
+  }
+
+  logout = async () => {
+    await firebase.auth().signOut()
+    this.setState({ user: null })
+    console.log('Déconnecté')
+  }
+
+  handleChange = event => {
+    const { name, value } = event.target
+    this.setState({ [name]: value })
+  }
+
+  handleSubmit = event => {
+    event.preventDefault()
+
+    this.signIn();
+  }
+
+  render() {
+    let data = ''
+
+    if (this.state.user) {
+      data = (
+        <Fragment>
+          <p>Bonjour {this.state.user.email}</p>
+          <button onClick={this.logout}>Se déconnecter</button>
+        </Fragment>
+      )
+    } else {
+      data = (
+        <form onSubmit={this.handleSubmit}>
+          <input value={this.state.email} onChange={this.handleChange} name='email' type='text' placeholder='email' /><br />
+          <input value={this.state.password} onChange={this.handleChange} name='password' type='password' placeholder='password' /><br />
+          <button type='submit'>Se connecter</button>
+        </form>
+      )
+    }
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          {data}
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+firebase.initializeApp({
+  apiKey: "AIzaSyCetyfrjFWwKsZtioZadMOtYNtwXNl-y7Q",
+  authDomain: "fir-base-86362.firebaseapp.com",
+  databaseURL: "https://fir-base-86362.firebaseio.com",
+  projectId: "fir-base-86362",
+  storageBucket: "",
+  messagingSenderId: "697408356990",
+  appId: "1:697408356990:web:b0b6df1dfd1613ba"
+});
+
+export default firebase;
